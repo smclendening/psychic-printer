b281583be296331d1381917b3a66f834aaf0937b
Scott <smclendening@gmail.com>
2017-09-22 13:28:53 -0700

    Initial implementation of sync sorted merge

--- solution/sync-sorted-merge.js
+++ solution/sync-sorted-merge.js
@@ -2,35 +2,55 @@
 
 const oldestLogBySource = (logSource, index) => {
   const log = logSource.pop();
-  return Object.assign(log, { index });
+  return log ? Object.assign(log, { index }) : false;
 }
 
 const byDate = (a, b) => {
   return a.date - b.date;
 }
 
-const merge = (log, logList, start, end) => {
-  // if log is older than start, return new array with log at beginning
+const merge = (log, logList) => {
+  const start = 0;
+  const end = logList.length - 1;
 
-  // if log is younger than end, return new array with log at end 
+  if (logList.length === 0) {
+    return [log];
+  }
+
+  if (log.date <= logList[start].date) {
+    return [log, ...logList];
+  }
+
+  if (log.date >= logList[end].date) {
+    return [...logList, log];
+  }
+
+  const midIndex = Math.max(1, Math.floor((end - start) / 2)); 
+
+  if (log.date < logList[midIndex].date) {
+    const mergeLeft = merge(log, logList.slice(start, midIndex));
+    return [...mergeLeft, ...logList.slice(midIndex)];
+  } else {
+    const mergeRight = merge(log, logList.slice(midIndex));
+    return [...logList.slice(start, midIndex), ...mergeRight]; 
+  }
 }
 
 module.exports = (logSources, printer) => {
-  const oldestLogs = logSources.map(oldestLogBySource).sort(byDate);
+  let oldestLogs = logSources.map(oldestLogBySource).sort(byDate);
 
-  // while oldestLogs length > 0
   while (oldestLogs.length > 0) {
-    // retrieve oldestLog and print
     const oldestLog = oldestLogs.shift();
     const sourceIndex = oldestLog.index;
+
     printer.print(oldestLog);
-    // retrieve next oldest log from that index
+
     const nextLogFromSource = oldestLogBySource(logSources[sourceIndex], sourceIndex);
 
     if (nextLogFromSource) {
-      // merge next oldest log with oldestLogs
-      oldestLogs = merge(nextLogFromSource, oldestLogs, 0, oldestLogs.length - 1);
+      oldestLogs = merge(nextLogFromSource, oldestLogs);
     }
   }
- // printer.done();
+ 
+  printer.done();
 }
