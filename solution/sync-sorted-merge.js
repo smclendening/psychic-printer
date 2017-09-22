bd29a3c356bcead993725c1bed89882bafe6a2e1
Scott <smclendening@gmail.com>
2017-09-22 11:34:50 -0700

    Refactor oldestLogBySource; begin implementing merge
    what happened
    
    [ticket: X]

--- solution/sync-sorted-merge.js
+++ solution/sync-sorted-merge.js
@@ -1,15 +1,36 @@
 'use strict'
 
-const oldestEntryBySource = (logSource, index) => {
-  const entry = logSource.pop();
-  return {entry, index};
+const oldestLogBySource = (logSource, index) => {
+  const log = logSource.pop();
+  return Object.assign(log, { index });
 }
 
 const byDate = (a, b) => {
-  return a.entry.date - b.entry.date;
+  return a.date - b.date;
+}
+
+const merge = (log, logList, start, end) => {
+  // if log is older than start, return new array with log at beginning
+
+  // if log is younger than end, return new array with log at end 
 }
 
 module.exports = (logSources, printer) => {
-  const oldestLogs = logSources.map(oldestEntryBySource).sort(byDate);
+  const oldestLogs = logSources.map(oldestLogBySource).sort(byDate);
 
+  // while oldestLogs length > 0
+  while (oldestLogs.length > 0) {
+    // retrieve oldestLog and print
+    const oldestLog = oldestLogs.shift();
+    const sourceIndex = oldestLog.index;
+    printer.print(oldestLog);
+    // retrieve next oldest log from that index
+    const nextLogFromSource = oldestLogBySource(logSources[sourceIndex], sourceIndex);
+
+    if (nextLogFromSource) {
+      // merge next oldest log with oldestLogs
+      oldestLogs = merge(nextLogFromSource, oldestLogs, 0, oldestLogs.length - 1);
+    }
+  }
+ // printer.done();
 }
