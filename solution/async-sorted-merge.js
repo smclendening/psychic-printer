33b8d85b7a33d7bb7ec2c59a48e98b98420e4679
Scott <smclendening@gmail.com>
2017-09-22 16:34:37 -0700

    Add error handling

--- solution/async-sorted-merge.js
+++ solution/async-sorted-merge.js
@@ -1,34 +1,53 @@
 'use strict'
 
-const byDate = require('./sync-sorted-merge').byDate;
-const merge = require('./sync-sorted-merge').merge;
+const { byDate, merge } = require('./sync-sorted-merge')
 
 const oldestLogBySourceAsync = async (logSource, index) => {
-  const log = await logSource.popAsync()
-  return log ? Object.assign(log, { index }) : false;
+  try {
+    const log = await logSource.popAsync()
+    return log ? Object.assign(log, { index }) : false;
+  }
+
+  catch(error) {
+    throw error;
+  }
 }
 
 const retrieveOldestLogs = async asyncPops => {
-  const logs = await Promise.all(asyncPops)
-  return logs.filter(log => log !== false).sort(byDate);
+  try {
+    const logs = await Promise.all(asyncPops)
+    return logs.filter(log => log !== false).sort(byDate);
+  }
+
+  catch(error) {
+    throw error;
+  }
 }
 
 module.exports = async (logSources, printer) => {
 
-  let oldestLogs = await retrieveOldestLogs(logSources.map(oldestLogBySourceAsync))
+  try {
+    let oldestLogs = await retrieveOldestLogs(logSources.map(oldestLogBySourceAsync))
 
-  while (oldestLogs.length > 0) {
-    const oldestLog = oldestLogs.shift();
-    const sourceIndex = oldestLog.index;
+    while (oldestLogs.length > 0) {
+      const oldestLog = oldestLogs.shift();
+      const sourceIndex = oldestLog.index;
 
-    printer.print(oldestLog);
+      printer.print(oldestLog);
 
-    const nextLogFromSource = await oldestLogBySourceAsync(logSources[sourceIndex], sourceIndex);
+      const nextLogFromSource = await oldestLogBySourceAsync(logSources[sourceIndex], sourceIndex);
 
-    if (nextLogFromSource) {
-      oldestLogs = merge(nextLogFromSource, oldestLogs);
+      if (nextLogFromSource) {
+        oldestLogs = merge(nextLogFromSource, oldestLogs);
+      }
     }
+    
+  } 
+
+  catch (error) {
+    console.error(error);
   }
+
 
   printer.done();
 }
