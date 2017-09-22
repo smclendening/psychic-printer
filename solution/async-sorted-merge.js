cbf05cdb85ffe1143d45ec54a44f9444d7d7b947
Scott <smclendening@gmail.com>
2017-09-22 15:38:09 -0700

    Initial implementation using async await

--- solution/async-sorted-merge.js
+++ solution/async-sorted-merge.js
@@ -1,5 +1,34 @@
 'use strict'
 
-module.exports = (logSources, printer) => {
-	throw new Error('Not implemented yet!  That part is up to you!')
+const byDate = require('./sync-sorted-merge').byDate;
+const merge = require('./sync-sorted-merge').merge;
+
+const oldestLogBySourceAsync = async (logSource, index) => {
+  const log = await logSource.popAsync()
+  return log ? Object.assign(log, { index }) : false;
+}
+
+const retrieveOldestLogs = async asyncPops => {
+  const logs = await Promise.all(asyncPops)
+  return logs.filter(log => log !== false).sort(byDate);
+}
+
+module.exports = async (logSources, printer) => {
+
+  let oldestLogs = await retrieveOldestLogs(logSources.map(oldestLogBySourceAsync))
+
+  while (oldestLogs.length > 0) {
+    const oldestLog = oldestLogs.shift();
+    const sourceIndex = oldestLog.index;
+
+    printer.print(oldestLog);
+
+    const nextLogFromSource = await oldestLogBySourceAsync(logSources[sourceIndex], sourceIndex);
+
+    if (nextLogFromSource) {
+      oldestLogs = merge(nextLogFromSource, oldestLogs);
+    }
+  }
+
+  printer.done();
 }
